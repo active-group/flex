6d378a7624b9ee2fdf53530d2360ca7e700d68d5
Mike Sperber <sperber@deinprogramm.de>
2023-03-06 13:49:10 +0100

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -161,6 +161,7 @@ list2a() -> [7, 5].
 list3() -> [2 | list2a()].
 
 % für jeden Fall einer Fallunterscheidung 1 Gleichung
+-spec list_sum(list(number())) -> number().
 list_sum([]) -> 0;
 list_sum([First | Rest]) -> 
     First  + list_sum(Rest). % rekursiven Aufruf auf dem Selbstbezug
