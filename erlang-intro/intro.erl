f0c28b6c938bc66357c7484f116a82a9eb78574f
Mike Sperber <sperber@deinprogramm.de>
2023-03-06 10:13:54 +0100

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -37,9 +37,13 @@ is_cute(snake) -> false.
 
 % Aggregatzustand von Wasser
 -spec water_state(number()) -> solid | liquid | gas.
-water_state(Temperature) ->
-    if
-        Temperature < 0 -> solid;
-        Temperature > 100 -> gas;
-        true -> liquid
-    end.
+% water_state(Temperature) ->
+%    if
+%        Temperature < 0 -> solid;
+%        Temperature > 100 -> gas;
+%        true -> liquid
+%    end.
+% 
+water_state(Temperature) when Temperature < 0 -> solid;
+water_state(Temperature) when Temperature > 100 -> gas;
+water_state(Temperature) -> liquid.
