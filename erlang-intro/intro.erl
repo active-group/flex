cbff6a442557c71e7df42e65910da3d7775f6b52
Mike Sperber <sperber@deinprogramm.de>
2023-03-06 11:43:33 +0100

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -6,7 +6,9 @@
          t1/0, t2/0, t_error/0,
          minutes_since_midnight/1,
          d1/0, d2/0, 
-         run_over_dillo/1]).
+         run_over_dillo/1,
+         p1/0, p2/0,
+         animal_weight/1]).
 
 % . fertig
 % ; "oder"
