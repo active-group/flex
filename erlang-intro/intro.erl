d31a2b51fc99a2344e07d139a529d4c9dc49b07c
Mike Sperber <sperber@deinprogramm.de>
2023-04-24 10:49:58 +0200

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -9,9 +9,19 @@
 double(X) -> X * 2.
 
 % Atom: "konstante Zeichenkette" / Name
-% 
+
 
 % Haustier ist eins der folgenden:
 % - Hund - ODER -
 % - Katze - ODER -
 % - Schlange
+
+% Ist ein Haustier niedlich?
+-spec is_cute(dog | cat | snake) -> boolean().
+is_cute(Pet) ->
+    case Pet of
+        dog -> true;
+        cat -> true;
+        snake -> false
+    end.
+
