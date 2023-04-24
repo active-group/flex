08b600d23fc83a818086967a672a38016aaccd96
Mike Sperber <sperber@deinprogramm.de>
2023-04-24 15:22:53 +0200

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -181,4 +181,4 @@ list_sum([First | Rest]) ->
 list_nth([], 0) -> {error, list_too_short};
 list_nth([First | Rest], 0) -> First;
 list_nth([], N) -> {errot, list_too_short};
-list_nth([First, Rest], N) -> undefined.
+list_nth([First | Rest], N) -> undefined.
