535276f37084bc26bd9ccee2b66b54cfa4a006d2
Mike Sperber <sperber@deinprogramm.de>
2023-09-18 14:31:09 +0200

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -205,3 +205,6 @@ highway() -> [dillo1(), dillo2(), parrot1(), parrot2()].
 run_over_animals([]) -> [];
 run_over_animals([First|Rest]) ->
     [run_over_animal(First) | run_over_animals(Rest)].
+
+% Funktion, welche aus einer Liste alle geraden Elemente extrahiert.
+% mit Hilfsfunktion, die testet, ob eine Zahl gerade ist oder nicht.
