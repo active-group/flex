4c1018320cdd7598d2e9bba16f813d2f001bdae6
Mike Sperber <sperber@deinprogramm.de>
2023-09-18 16:45:25 +0200

    Update.

--- erlang-intro/process.erl
+++ erlang-intro/process.erl
@@ -1,1 +1,9 @@
 -module(process).
+
+process_code() ->
+    receive % Syntax ist wie case
+        "Mike" ->
+            io:format("Mike ist da.~n"),
+            process_code();
+        "Sperber" -> io:format("Sperber ist doof.~n")
+    end.
