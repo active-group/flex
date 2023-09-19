554af7889e9080df83f5fc575ecf27077cc86691
Mike Sperber <sperber@deinprogramm.de>
2023-09-19 09:40:01 +0200

    Update.

--- erlang-intro/counter.erl
+++ erlang-intro/counter.erl
@@ -13,6 +13,11 @@ init(N) -> {ok, N}.
 counter_get(Pid) ->
     gen_server:call(Pid, {get, self()}).
 
+counter_inc(Pid) ->
+    gen_server:cast(Pid, inc).
+counter_inc(Pid, Inc) ->
+    gen_server:cast(Pid, {inc, Inc}).
+
 handle_cast(inc, State) ->
     {noreply, State + 1};
 handle_cast({inc, Inc}, State) ->
