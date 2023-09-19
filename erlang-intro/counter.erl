c8f207fd394ddd890b801f72b7076b7a133333ce
Mike Sperber <sperber@deinprogramm.de>
2023-09-19 09:34:46 +0200

    Update.

--- erlang-intro/counter.erl
+++ erlang-intro/counter.erl
@@ -4,7 +4,10 @@
 -behavior(gen_server).
 
 -type state() :: number().
+-type message() :: inc | {inc, number()} | {get, pid()}.
 
 -spec init(number()) -> {ok, state()}.
 init(N) -> {ok, N}.
 
+counter_get(Pid) ->
+    gen_server:call(Pid, {get, self()}).
