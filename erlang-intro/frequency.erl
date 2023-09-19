9997f352175183afabf68c52a3a78a6b7ab969f8
Mike Sperber <sperber@deinprogramm.de>
2023-09-19 10:22:33 +0200

    Update.

---
+++ erlang-intro/frequency.erl
@@ -0,0 +1,9 @@
+-module(frequency).
+
+% Aufgabe:
+% gen_server schreiben, der Frequenzen / Zahlen
+% "vergeben" kann.
+% Der Server beginnt mit einer Liste von Zahlen.
+% Man kann eine Zahl "ziehen".
+% Eine Zahl wird nicht 2x vergeben.
+% Außerdem kann man Zahl wieder zurückgeben.
