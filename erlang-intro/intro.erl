cfb2a4e92bda4a2b627d6e21b6f09005d5f0b1a1
Mike Sperber <sperber@deinprogramm.de>
2024-06-10 11:51:41 +0200

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -100,4 +100,9 @@ safe_divide(A, B) ->
     end.
 
 % Steigung einer Geraden berechnen
--spec slope(position(), position()) -> number().
+-spec slope(position(), position()) -> number() | vertical.
+slope({X1, Y1}, {X2, Y2}) ->
+    case safe_divide(X2-X1, Y2-Y1) of
+        divide_by_zero -> vertical;
+        {ok, Slope} -> Slope
+    end.
