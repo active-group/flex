7f059787c35235da7e7232ded8ea8e033b5aec91
Mike Sperber <sperber@deinprogramm.de>
2024-06-10 15:11:30 +0200

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -214,9 +214,9 @@ list_double([]) -> [];
 list_double([First|Rest]) ->
     [ double(First) | list_double(Rest) ].
 
-list_map([]) -> [];
-list_map([First|Rest]) ->
-    [ F(First) | list_map(Rest) ].
+list_map(F, []) -> [];
+list_map(F, [First|Rest]) ->
+    [ F(First) | list_map(F, Rest) ].
 
 % Binary:
 % <<"Mike">>
