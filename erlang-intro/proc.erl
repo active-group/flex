8ea72d96b58e0e83bbbc64814affca06e593c56e
Mike Sperber <sperber@deinprogramm.de>
2024-06-10 16:30:18 +0200

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,10 +1,12 @@
 -module(proc).
--export([format_server/0, start_format_server/p]).
+-export([format_server/0, start_format_server/0]).
 
 format_server() ->
     receive
         Message -> io:format(Message),
                    format_server()
+        after 10000 ->
+            io:format("timeout")
     end.
 
 start_format_server() ->
