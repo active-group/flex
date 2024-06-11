50e61cbd2e8622b5b3dac8b1775dc72f2ea20281
Mike Sperber <sperber@deinprogramm.de>
2024-06-11 11:06:52 +0200

    Update.

--- erlang-intro/inc_server.erl
+++ erlang-intro/inc_server.erl
@@ -1,2 +1,9 @@
 -module(inc_server).
 -behaviour(gen_server).
+
+% gen_server: Prozeß, der Zustand verwaltet und Messages entgegennimmt.
+
+-record(inc, {increment :: number()}).
+-record(get, { client_pid :: pid() }). % RPC
+
+-type inc_server_message() :: #inc{} | #get{}.
