ff1faaa4ffb4edc168ab3da25ac0da5d28f41620
Mike Sperber <sperber@deinprogramm.de>
2024-06-11 11:09:29 +0200

    Update.

--- erlang-intro/inc_server.erl
+++ erlang-intro/inc_server.erl
@@ -3,7 +3,14 @@
 
 % gen_server: Prozeß, der Zustand verwaltet und Messages entgegennimmt.
 
+% inc_server ...
+% Zustand: Zähler
+
 -record(inc, {increment :: number()}).
 -record(get, { client_pid :: pid() }). % RPC
 
 -type inc_server_message() :: #inc{} | #get{}.
+
+init(InitialN) -> % Anfangswert des Zählers
+    {ok, InitialN}. % erste Zustand
+
