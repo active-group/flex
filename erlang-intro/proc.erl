a923235b222e76aaeb39eff56be0c3d265dd936e
Mike Sperber <sperber@deinprogramm.de>
2024-12-09 16:21:19 +0100

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,6 +1,9 @@
 -module(proc).
-
+-export([number_start/1]).
 % Service, der eine Zahl verwaltet
+
+number_start(InitialN) ->
+    spawn(fun () -> number_loop(InitialN) end).
 
 number_loop(N) ->
     receive
