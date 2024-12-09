624c8bb9f7bc413c2284ac17beca36a9af4d2bec
Mike Sperber <sperber@deinprogramm.de>
2024-12-09 16:34:45 +0100

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,5 +1,6 @@
 -module(proc).
 -export([number_start/1,
+         number_add/2,
          number_query/1]).
 % Service, der eine Zahl verwaltet
 
@@ -12,12 +13,17 @@ number_query(Pid) ->
         N -> {ok, N}
     end.
 
+number_add(Pid, Inc) ->
+    Pid ! Inc.
 
 number_loop(N) ->
     receive
         {query, SenderPid} ->
             SenderPid ! N,
             number_loop(N);
+        % zusätzlich:
+        % - Multiplikation
+        % - Subtraktion
         Inc -> 
             io:format("old N: ~w~n", [N]),
             number_loop(N + Inc)
