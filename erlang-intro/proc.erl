d3e98ee5cdbfa41d488a75f2badb77e40ac3e535
Mike Sperber <sperber@deinprogramm.de>
2024-12-09 16:20:26 +0100

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,1 +1,10 @@
 -module(proc).
+
+% Service, der eine Zahl verwaltet
+
+number_loop(N) ->
+    receive
+        Inc -> 
+            io:format("old N: ~w~n", [N]),
+            number_loop(N + Inc)
+    end.
