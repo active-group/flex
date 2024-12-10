4fbc11f5ebe061f8b5c442a755858866945b045f
Mike Sperber <sperber@deinprogramm.de>
2024-12-10 11:19:02 +0100

    Update.

--- erlang-intro/number_server.erl
+++ erlang-intro/number_server.erl
@@ -1,1 +1,22 @@
 -module(number_server).
+
+-behavior(gen_server).
+
+-export([init/1,
+         handle_cast/2]).
+
+init(InitialN) ->
+    {ok, InitialN}.
+
+% cast: asynchrone Nachricht an den Server
+% call: RPC mit Antwort
+
+% Cast:
+-record(add, { inc :: number()}).
+
+handle_cast(#add{ inc = Inc}, N) ->
+    {noreply, N + Inc}.
+
+
+% Call:
+-record(query, { sender_pid :: pid() }).
