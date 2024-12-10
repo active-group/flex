0915b1d70b08331bdb473b859b9379077cc099d2
Mike Sperber <sperber@deinprogramm.de>
2024-12-10 11:25:18 +0100

    Update.

--- erlang-intro/number_server.erl
+++ erlang-intro/number_server.erl
@@ -5,7 +5,9 @@
 -export([init/1,
          handle_cast/2,
          handle_call/3,
-         start/1]).
+         start/1,
+         add/2,
+         query/1]).
 
 start(InitialN) ->
     gen_server:start(number_server, InitialN, []).
@@ -19,12 +21,18 @@ init(InitialN) ->
 % Cast:
 -record(add, { inc :: number()}).
 
+add(Pid, Inc) ->
+    gen_server:cast(Pid, #add { inc = Inc }).
+
 handle_cast(#add{ inc = Inc}, N) ->
     {noreply, N + Inc}.
 
 
 % Call:
 -record(query, {}).
+
+query(Pid) ->
+    gen_server:call(Pid, #query{}).
 
 handle_call(#query {}, _From, N) ->
     {reply,
