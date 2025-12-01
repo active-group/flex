81deb82f1094486e8e16f7361f37710fe6466776
Mike Sperber <sperber@deinprogramm.de>
2025-12-01 16:59:03 +0100

    Update.

--- erlang-intro/number_server.erl
+++ erlang-intro/number_server.erl
@@ -1,5 +1,5 @@
 -module(number_server).
--export([init/1, handle_cast/2]).
+-export([init/1, handle_cast/2, handle_call/3]).
 
 % Server mit Zustand
 -behavior(gen_server).
@@ -20,3 +20,8 @@ handle_cast(#increment { inc = Inc}, N) ->
     {noreply, N + Inc};
 handle_cast(#multiply { factor = Factor}, N) ->
     {noreply, N * Factor}.
+
+handle_call(#query { pid = SenderPid}, From, N) ->
+    {reply,
+     N, % Antwort
+     N}. % neuer Zustand
