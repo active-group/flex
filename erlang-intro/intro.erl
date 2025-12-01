6ac219cd5e2649ef98e5fa23d957076ded4d508b
Mike Sperber <sperber@deinprogramm.de>
2025-12-01 13:55:29 +0100

    Update.

--- erlang-intro/intro.erl
+++ erlang-intro/intro.erl
@@ -191,3 +191,6 @@ highway() -> [dillo1(), dillo2(), parrot1(), parrot2()].
 run_over_animals([]) -> [];
 run_over_animals([First | Rest]) ->
     [ run_over_animal(First) | run_over_animals(Rest)].
+
+-spec is_even(number()) -> number().
+is_even(X) -> X rem 2 == 0.
