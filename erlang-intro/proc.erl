85f5d3dbfdaf304d4dfdfb34b41560d50ffcc9ee
Mike Sperber <sperber@deinprogramm.de>
2025-12-01 16:09:59 +0100

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,14 +1,14 @@
 -module(proc).
--export([number_server/1, number_inc/2,
+-export([number_server/1, number_inc/2, number_multiply/2,
          number_query/1]).
 
 number_server(InitialN) ->
     spawn(fun () -> number_loop(InitialN) end).
 
 % Zahl erhöhen
--spec number_inc(pid(), number()) -> any().
-number_inc(Pid, Inc) ->
-    Pid ! Inc.
+%-spec number_inc(pid(), number()) -> any().
+%number_inc(Pid, Inc) ->
+%    Pid ! Inc.
 
 % Zahl mit Faktor multiplizieren
 
@@ -18,19 +18,34 @@ number_inc(Pid, Inc) ->
 % - abfragen
 
 -record(query, { pid :: pid() }).
+-record(multiply, { factor :: number() }).
+-record(increment, { inc :: number() }).
 
 -spec number_query(pid()) -> number().
 number_query(Pid) ->
     Pid ! #query { pid = self() }, % self(): eigene Pid
     receive N -> N end.
 
+-spec number_multiply(pid(), number()) -> any().
+number_multiply(Pid, Factor) ->
+    Pid ! #multiply { factor = Factor }.
+
+-spec number_inc(pid(), number()) -> any().
+number_inc(Pid, Inc) ->
+    Pid ! #increment { inc = Inc}.
+
 % Brauchen Funktion mit Zustand als Parameter
+-spec number_loop(number()) -> no_return().
 number_loop(N) ->
     receive
         #query { pid = SenderPid } -> % brauche Pid des Absenders
             SenderPid ! N,
             number_loop(N);
-        Inc -> 
-            io:format("N: ~p, Inc: ~p\n", [N, Inc]),
+        #multiply { factor = Factor } ->
+            number_loop(N * Factor);
+        #increment { inc = Inc} ->
             number_loop(N + Inc)
+%        Inc -> 
+%            io:format("N: ~p, Inc: ~p\n", [N, Inc]),
+%            number_loop(N + Inc)
     end.
