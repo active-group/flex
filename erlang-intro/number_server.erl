949e27c22210348d4d8bcc13af4aa65577bc15af
Mike Sperber <sperber@deinprogramm.de>
2025-12-01 16:53:59 +0100

    Update.

--- erlang-intro/number_server.erl
+++ erlang-intro/number_server.erl
@@ -6,4 +6,12 @@
 
 -type state() :: number().
 
+% Messages:
+% will Antwort: "call"
+-record(query, { pid :: pid() }).
+
+% will keine Antwort: "cast"
+-record(multiply, { factor :: number() }).
+-record(increment, { inc :: number() }).
+
 init(InitialN) -> {ok, InitialN}.
