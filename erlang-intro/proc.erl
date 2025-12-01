b37e215cf9ddb95f90ec9ff6b61d685e29d06d78
Mike Sperber <sperber@deinprogramm.de>
2025-12-01 15:47:56 +0100

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,5 +1,6 @@
 -module(proc).
--export([number_server/1, number_inc/2]).
+-export([number_server/1, number_inc/2,
+         number_query/1]).
 
 number_server(InitialN) ->
     spawn(fun () -> number_loop(InitialN) end).
@@ -14,11 +15,20 @@ number_inc(Pid, Inc) ->
 % - hochzählen
 % - abfragen
 
+-record(query, { pid :: pid() }).
+
+-spec number_query(pid()) -> number().
+number_query(Pid) ->
+    Pid ! #query { pid = self() }, % self(): eigene Pid
+    receive N -> N end.
+
 % Brauchen Funktion mit Zustand als Parameter
 number_loop(N) ->
     receive
+        #query { pid = SenderPid } -> % brauche Pid des Absenders
+            SenderPid ! N,
+            number_loop(N);
         Inc -> 
             io:format("N: ~p, Inc: ~p\n", [N, Inc]),
-            number_loop(N + Inc);
-        query -> todo
+            number_loop(N + Inc)
     end.
