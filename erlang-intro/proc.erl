eb0cbd25035bf26d2ee99d9db7f91993a17e5bdf
Mike Sperber <sperber@deinprogramm.de>
2025-12-01 15:32:39 +0100

    Update.

--- erlang-intro/proc.erl
+++ erlang-intro/proc.erl
@@ -1,1 +1,18 @@
 -module(proc).
+-export([number_server/1]).
+
+number_server(InitialN) ->
+    spawn(fun () -> number_loop(InitialN) end).
+
+% Prozeß, der eine Zahl (Zustand) verwaltet
+% Operationen:
+% - hochzählen
+% - abfragen
+
+% Brauchen Funktion mit Zustand als Parameter
+number_loop(N) ->
+    receive
+        Inc -> 
+            io:format("N: ~p, Inc: ~p\n", [N, Inc]),
+            number_loop(N + Inc)
+    end.
