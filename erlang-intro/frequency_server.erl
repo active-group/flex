1d1daef9bcf59ac681d2461b96d318cc639a9032
Mike Sperber <sperber@deinprogramm.de>
2025-12-02 09:29:59 +0100

    Update.

--- erlang-intro/frequency_server.erl
+++ erlang-intro/frequency_server.erl
@@ -1,1 +1,6 @@
 -module(frequency_server).
+
+% Schreibe einen gen_server, der eine Menge von Frequenzen verwaltet.
+% Operationen:
+% - man kann sich eine freie Frequenz geben lassen (wenn's eine gibt)
+% - man kann eine Frequenz auch wieder zurückgeben
