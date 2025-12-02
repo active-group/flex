e2f6e01e37fcbecb9d1aa7a3547d77630ad031c1
Mike Sperber <sperber@deinprogramm.de>
2025-12-02 09:15:56 +0100

    Update.

--- erlang-intro/number_server.erl
+++ erlang-intro/number_server.erl
@@ -1,5 +1,6 @@
 -module(number_server).
--export([init/1, handle_cast/2, handle_call/3]).
+-export([init/1, handle_cast/2, handle_call/3,
+         start/0]).
 
 % Server mit Zustand
 -behavior(gen_server).
@@ -14,7 +15,23 @@
 -record(multiply, { factor :: number() }).
 -record(increment, { inc :: number() }).
 
+start() ->
+    gen_server:start(number_server, 7, []). % Module, Args, Options
+
+% bekommt das, was an gen_server:start übergeben wird
+
+% wird ausgeführt im Server-Prozeß   -> self()
+-spec init(number()) -> {ok, state()}.
 init(InitialN) -> {ok, InitialN}.
+
+-spec number_inc(pid(), number()) -> any().
+number_inc(Pid, Inc) ->
+    % wichtig: ! funktioniert nicht direkt!
+    gen_server:cast(Pid, #increment { inc = Inc }).
+
+-spec number_multiply(pid(), number()) -> any().
+number_multiply(Pid, Factor) ->
+    gen_server:cast(Pid, #multiply { factor = Factor }).
 
 handle_cast(#increment { inc = Inc}, N) -> 
     {noreply, N + Inc};
